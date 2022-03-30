1ab03dc7139f781d94e19f9a68f284fd26c15add
Tiffani <TiffaniL95@gmail.com>
2022-03-30 13:04:09 -0700

    updated project

--- server/app.js
+++ server/app.js
@@ -1,10 +1,36 @@
 // import files and packages up here
+const express = require('express')
+const morgan = require('morgan')
+const topSpotsData = require('../server/data.json');
 
 
 // create your express server below
-var app;
+const app = express();
 
 // add your routes and middleware below
+app.use(morgan('dev'))
+
+app.get('/data', function(req,res, next){
+    
+    res.json(topSpotsData);
+
+    next();
+});
+
+
+app.get('/', function(req, res){
+
+    res.status(200).send('hello');
+
+    next();
+
+});
+
+app.get('*', function(req, res){
+
+    res.end('Not Found');
+
+})
 
 // finally export the express application
 module.exports = app;
